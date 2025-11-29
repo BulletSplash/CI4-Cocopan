57dfe3ae4164eae82d611361a7fd52d991996265
Lawrence Blasco <79191129+BulletSplash@users.noreply.github.com>
2025-11-29 20:42:37 +0800

    feat(db tables): Added a migration for users, products, transactions table

---
+++ backend/app/Database/Migrations/2025-11-29-101257_CreateUsersTable.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'users_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'full_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+                'unique'     => true,
+            ],
+            'password_hash' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'type' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+                'default'    => 'client',
+                'null'       => false,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('users_id', true);
+        $this->forge->createTable('users', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users', true);
+    }
+}
