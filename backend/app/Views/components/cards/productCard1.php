a319a8f56a1e8ec40f8e4749afe3ef879f6ffdfb
Lawrence Blasco <79191129+BulletSplash@users.noreply.github.com>
2025-11-29 22:35:56 +0800

    refactor(auth): Updated Auth Controller with following functions and expose Routes
    - Login = Auth::login
    - Logout = Auth::logout
    - Sign Up = Auth::signup

--- backend/app/Views/components/cards/productCard1.php
+++ backend/app/Views/components/cards/productCard1.php
@@ -1,4 +1,4 @@
-<a href="<?= esc($link ?? "") ?>" ><div class="bg-[#151522]/70 border border-white/10 rounded-xl shadow-xl backdrop-blur-xl hover:scale-105 transition">
+<a href="<?= esc($link ?? "/") ?>" ><div class="bg-[#151522]/70 border border-white/10 rounded-xl shadow-xl backdrop-blur-xl hover:scale-105 transition">
   <div class="flex justify-center items-center">
         <img src="<?= esc($image ?? 'https://images.squarespace-cdn.com/content/v1/670617fd9fdb0f1a8f5fbffe/30a4aed0-e246-4baa-82fd-5327ca4d4ef9/Breadbox_ISO2.png') ?>"
              class="m-2 object-contain w-full h-56 object-cover opacity-80" alt="Bread">
--- backend/app/Views/components/header.php
+++ backend/app/Views/components/header.php
@@ -7,40 +7,20 @@
     <div class="flex items-center space-x-6">
       <a href='/' class="flex items-center space-x-2 hover:opacity-90 transition">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRheI89iFUg5I_sVIDYpD_Q2HPxPAfNzebhgA&s" 
-             alt="Cocopan Logo" 
-             class="w-10 h-10 rounded-full object-cover shadow-lg">
+            alt="Cocopan Logo" 
+            class="w-10 h-10 rounded-full object-cover shadow-lg">
         <h1 class="text-2xl font-bold text-purple-300">Cocopan</h1>
       </a>
     </div>
 
-    <?php if (empty($session->get('id'))): ?>
-    <div class="space-x-4">
-
-      <a href="login" >
-        <?= view('components/buttons/secondary', ['title' => "Log In"]) ?>
-      </a>
-
-      <a href="register">
-        <?= view('components/buttons/primary', ['title' => "Sign Up"]) ?>
-      </a>
-
+    <div>
+      <?php if(empty($session->get('id'))): ?>
+        <?= view('components/buttons/secondary_link', ['link' => '/login', 'title' => 'Log In']) ?>
+        <?= view('components/buttons/primary_link', ['link' => '/register', 'title' => 'Sign In']) ?>
+      <?php else:  ?>  
+        <span> Welcome! <?= esc($session->get('full_name')) ?> </span>
+        <?= view('components/buttons/secondary_link', ['link' => '/logout', 'title' => 'Log Out']) ?>
+      <?php endif; ?>
     </div>
-
-    <?php else: ?>
-    <div class="space-x-4 flex items-center">
-
-      <p class="mt-2 text-purple-300">
-        <?= esc($session->get('display_name')) ?>
-      </p>
-
-      <a href="logout" 
-         class="bg-purple-500 hover:bg-purple-600 text-black px-4 py-2 rounded-full font-semibold transition">
-        Log Out
-      </a>
-
-    </div>
-    <?php endif; ?>
-
   </nav>
-
 </header>
